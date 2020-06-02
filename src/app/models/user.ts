ad547ccb8998b41519d8f6d9e08cd952f4a64a3a
pedrobcandoso@gmail.com <pedrobcandoso@gmail.com>
2020-06-02 10:12:50 +0100

    Static product seller information

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,9 +1,16 @@
 export class User {
-  id : number;
-  firstName : string;
-  lastName : string;
-  email : string;
-  password : string;
-  token : string;
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  token: string;
+
+  constructor(id: number, firstName: string, lastName: string, email: string) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
 
 }
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ApiService} from '../services/api.service';
 import {Product} from '../models/product';
+import {User} from '../models/user';
 
 @Component({
   selector: 'app-product-details',
@@ -11,10 +12,12 @@ import {Product} from '../models/product';
 export class ProductDetailsComponent implements OnInit {
 
   prod: Product;
+  seller: User;
 
   constructor(private route: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit(): void {
+    this.seller = new User(1, 'John', 'Smith', 'smith@mail.com');
     const id = this.route.snapshot.paramMap.get('id');
     // change paths
     this.api.get('/products/id=' + id).subscribe(data => {
