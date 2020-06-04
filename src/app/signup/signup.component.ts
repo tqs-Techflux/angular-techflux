216d20842c18c5ee7520148ca543d50f2bdfbec2
pedrobcandoso@gmail.com <pedrobcandoso@gmail.com>
2020-06-04 21:04:07 +0100

    Form validations and sign up functional

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {SidebarService} from "../services/sidebar.service";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {SidebarService} from '../services/sidebar.service';
+import {UserService} from '../services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -8,32 +10,61 @@ import {SidebarService} from "../services/sidebar.service";
   styleUrls: ['./signup.component.css']
 })
 
-export class SignupComponent implements OnInit,OnDestroy {
+export class SignupComponent implements OnInit, OnDestroy {
 
   signupForm: FormGroup;
 
   constructor(public fb: FormBuilder,
-              private sidebarService: SidebarService
+              private sidebarService: SidebarService,
+              private userService: UserService,
+              private router: Router
   ) {
     this.signupForm = this.fb.group({
-      firstname: [''],
-      lastname: [''],
-      email: [''],
-      password: ['']
+      fName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      lName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      contact: ['']
     });
   }
 
   ngOnInit(): void {
     this.sidebarService.sidebar = false;
-    document.getElementById("sidebarCollapse").hidden = true;
+    document.getElementById('sidebarCollapse').hidden = true;
   }
 
   ngOnDestroy(): void {
-    document.getElementById("sidebarCollapse").hidden = false;
+    document.getElementById('sidebarCollapse').hidden = false;
   }
 
-  doSignUp() {
-    alert("");
+  doSignUp(info) {
+    if (info.status === 'VALID') {
+      const formData = new FormData();
+      Object.keys(info.value).forEach((key) => { formData.append(key, info.value[key]); });
+      this.userService.signUp(formData).subscribe(() => {
+        this.router.navigate(['/']);
+      }, (err) => {
+        console.log('Failed to sign up');
+      });
+    }
   }
+
+  get firstname() { return this.signupForm.get('fName'); }
+  get lastname() { return this.signupForm.get('lName'); }
+  get password() { return this.signupForm.get('password'); }
+  get email() { return this.signupForm.get('email'); }
 
 }
