f64019bb731cf92c42e1995cd0997ce463208f73
pedrobcandoso@gmail.com <pedrobcandoso@gmail.com>
2020-06-04 21:23:56 +0100

    Fixed URL

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,7 +11,7 @@ import {DomSanitizer} from '@angular/platform-browser';
 })
 export class ApiService {
 
-  private url = 'https://techflux.herokuapp.com/';
+  private url = 'https://techflux.herokuapp.com';
 
   constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
 
